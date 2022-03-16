4d454edaf5ed1c2f28495999e1ca4cb642c1841f
Ovod18 <ovod18@yandex.ru>
2022-03-16 16:22:02 +0400

    Fixed project documentation.

--- space_shooter/space_shooter.py
+++ space_shooter/space_shooter.py
@@ -4,10 +4,6 @@ import random
 WIDTH = 480
 HEIGHT = 600
 FPS = 60
-"""The information line height.
-
-    |
-"""
 
 # Setting colors.
 WHITE = (255, 255, 255)
@@ -17,55 +13,63 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
-# Create the main window.
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Shmup!")
-clock = pygame.time.Clock()
+def main():
+    """The main function in space_shooter
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 40))
-        self.image.fill(GREEN)
-        self.rect = self.image.get_rect()
-        self.rect.centerx = WIDTH / 2
-        self.rect.bottom = HEIGHT - 10
-        self.speedx = 0
+    |
+    """
+    # Create the main window.
+    pygame.init()
+    pygame.mixer.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Space_shooter")
+    clock = pygame.time.Clock()
 
-    def update(self):
-        self.speedx = 0
-        keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_LEFT]:
-            self.speedx = -8
-        if keystate[pygame.K_RIGHT]:
-            self.speedx = 8
-        self.rect.x += self.speedx
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-        if self.rect.left < 0:
-            self.rect.left = 0
+    class Player(pygame.sprite.Sprite):
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.Surface((50, 40))
+            self.image.fill(GREEN)
+            self.rect = self.image.get_rect()
+            self.rect.centerx = WIDTH / 2
+            self.rect.bottom = HEIGHT - 10
+            self.speedx = 0
 
-all_sprites = pygame.sprite.Group()
-player = Player()
-all_sprites.add(player)
+        def update(self):
+            self.speedx = 0
+            keystate = pygame.key.get_pressed()
+            if keystate[pygame.K_RIGHT]:
+                self.speedx = 8
+            if keystate[pygame.K_LEFT]:
+                self.speedx = -8
+            self.rect.x += self.speedx
+            if self.rect.right > WIDTH:
+                self.rect.right = WIDTH
+            if self.rect.left < 0:
+                self.rect.left = 0
 
-# Create the game cycle.
-running = True
-while running:
-    clock.tick(FPS)
-    # Check events
-    for event in pygame.event.get():
-        # Check closing main window event
-        if event.type == pygame.QUIT:
-            running = False
+    all_sprites = pygame.sprite.Group()
+    player = Player()
+    all_sprites.add(player)
 
-    all_sprites.update()
+    # Create the game cycle.
+    running = True
+    while running:
+        clock.tick(FPS)
+        # Check events
+        for event in pygame.event.get():
+            # Check closing main window event
+            if event.type == pygame.QUIT:
+                running = False
 
-    # Rendering
-    screen.fill(BLACK)
-    all_sprites.draw(screen)
-    pygame.display.flip()
+        all_sprites.update()
 
-pygame.quit()
+        # Rendering
+        screen.fill(BLACK)
+        all_sprites.draw(screen)
+        pygame.display.flip()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
